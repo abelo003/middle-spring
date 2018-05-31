3503efdd9936a5a64ab3f3165d9745e9f5e6b0de
abelo003 <boxsof.live@hotmail.com>
2018-05-31 16:56:46 -0500

    Se agrega el consumer

--- src/main/java/com/test/middle/service/ProducerService.java
+++ src/main/java/com/test/middle/service/ProducerService.java
@@ -5,10 +5,15 @@
  */
 package com.test.middle.service;
 
+import com.test.middle.dto.ResponseDTO;
+import com.test.middle.dto.response.BitacoraResponse;
 import com.test.middle.model.BitacoraModel;
 import org.apache.log4j.Logger;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 /**
@@ -31,6 +36,11 @@ public class ProducerService {
     public void produceMsg(BitacoraModel msg) {
         amqpTemplate.convertAndSend(exchange, routingKey, msg);
         LOGGER.info("Se agrega el mensaje al broker.");
+    }
+    
+    @RabbitListener(queues="jsa.queue")
+    public void consumerBitacora(BitacoraModel msg){
+        LOGGER.info("Se consume el mensaje del broker: " + msg);
     }
 
 }
